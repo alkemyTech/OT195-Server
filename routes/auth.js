973241d2325d76dcdce03ea4d64c191c3cd78a43
Felipe Michalski <felimichalski@icloud.com>
2022-05-21 13:11:07 -0300

    Bugs fixed

--- routes/auth.js
+++ routes/auth.js
@@ -4,10 +4,6 @@ var router = express.Router()
 const { signUp } = require('../controllers/auth');
 const { registerValidator, checkValidator } = require('../middlewares/userValidate')
 
-router.get('/', (req, res) => {
-    res.send('hi')
-})
-
 router.post('/register', registerValidator, checkValidator, signUp)
 
 
