b352221581f5b0b2acd4efee7d05816f00fc5f3a
Felipe Michalski <felimichalski@icloud.com>
2022-05-23 23:35:35 -0300

    Port changed and info added to public endpoint

--- routes/organizations.js
+++ routes/organizations.js
@@ -5,11 +5,38 @@ var router = express.Router();
 router.get('/1/public', function (req, res, next) {
   // Public data
   const data = {
-    name: "",
-    image: "",
+    name: "Somos Mas",
+    image: "/images/logo.png",
     phone: "",
     address: "",
-    welcomeText: "",
+    welcomeText: "Hola! Bienvenidx",
+    news: [
+      {
+        image: '/images/latest-01.jpg',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur laborum distinctio magnam inventore dicta aut molestias dolorum harum ut.',
+        link: '/'
+      },
+      {
+        image: '/images/latest-02.jpg',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur laborum distinctio magnam inventore dicta aut molestias dolorum harum ut.',
+        link: '/'
+      },
+      {
+        image: '/images/latest-03.jpg',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur laborum distinctio magnam inventore dicta aut molestias dolorum harum ut.',
+        link: '/'
+      },
+      {
+        image: '/images/latest-04.jpg',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur laborum distinctio magnam inventore dicta aut molestias dolorum harum ut.',
+        link: '/'
+      },
+      {
+        image: '/images/latest-05.jpg',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur laborum distinctio magnam inventore dicta aut molestias dolorum harum ut.',
+        link: '/'
+      },
+    ]
   };
   res.status(200).json(data);
 });
