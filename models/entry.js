d875c629e785a0176f03eefeba23e8a766214b01
Sergio Rivera <sergiorive02@gmail.com>
2022-05-27 13:01:16 -0300

    feat: add entries model and associations

---
+++ models/entry.js
@@ -0,0 +1,29 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Entry extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Entry.belongsTo(models.Category, { as: "category" });
+    }
+  }
+  Entry.init(
+    {
+      name: DataTypes.STRING,
+      content: DataTypes.STRING,
+      image: DataTypes.STRING,
+      categoryId: DataTypes.INTEGER,
+      type: DataTypes.INTEGER,
+      deletedAt: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: "Entry",
+    }
+  );
+  return Entry;
+};
