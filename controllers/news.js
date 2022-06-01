526495291a952a277ec881ef74f0300354bd5a14
Sergio Rivera <sergiorive02@gmail.com>
2022-06-01 15:19:06 -0300

    fix: add 'idÂ' attribute to the response from the news

--- controllers/news.js
+++ controllers/news.js
@@ -26,7 +26,6 @@ const getNewsDetails = async (req, res) => {
   }
 };
 
-
 const createNews = async (req, res) => {
   try {
     const entry = await Entry.create({ ...req.body, type: "1" });
@@ -37,19 +36,19 @@ const createNews = async (req, res) => {
   }
 };
 
-const getNewsList = async(req, res) => {
+const getNewsList = async (req, res) => {
   try {
     const entries = await Entry.findAll({
-      attributes: ['name', 'image', 'createdAt']
+      attributes: ["name", "image", "id", "createdAt"],
     });
-    res.status(200).json({entries, ok: true});
+    res.status(200).json({ entries, ok: true });
   } catch (error) {
-    res.status(400).json({msg:error.message, ok: false});
+    res.status(400).json({ msg: error.message, ok: false });
   }
-}
+};
 
 module.exports = {
   getNewsDetails,
   createNews,
-  getNewsList
+  getNewsList,
 };
