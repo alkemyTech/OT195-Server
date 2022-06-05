d3b256bd8714297d23b281d431f4bc29ba8b7e08
edgardo90 <edgardo9000@gmail.com>
2022-06-05 05:34:47 -0300

    creacion del put de la ruta de testimonio por su id

--- controllers/tetimonies.js
+++ controllers/tetimonies.js
@@ -27,6 +27,30 @@ const createTestimony = async(req , res , next) =>{
         console.log(error);
         return res.status(500).json({ msg: "internal server error", ok: false });
     }
-} 
+}
 
-module.exports = {createTestimony } ; 
+const modifyTestimony = async(req , res , next) =>{
+    try{
+        const {id} = req.params;
+        const {name , content} = req.body;
+        let image // variable que va tener la imagen si hay en req.file
+        if(req.file){
+            image = req.file.filename
+        }
+        const allTestimonies = await Testimony.findAll()
+        const idTestimony = allTestimonies.find(el => el.id.toString() === id);
+        if(idTestimony){
+            idTestimony.name = name ? name : idTestimony.name;
+            idTestimony.content = content ? content : idTestimony.content;
+            idTestimony.image = image ? image : idTestimony.image;
+            return res.status(200).send(idTestimony);
+        }
+        return res.status(404).json({ error: "no se encuntra ese id" });
+    }catch(error){
+        next(error);
+        console.log(error);
+        return res.status(500).json({ msg: "internal server error", ok: false })
+    }
+}
+
+module.exports = {createTestimony , modifyTestimony } ; 
