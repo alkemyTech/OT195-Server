a11a321750210b3a7dc7e0cccb8b08d657a8f4c3
Felipe Michalski <felimichalski@icloud.com>
2022-06-06 14:47:00 -0300

    Code restructured

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,31 +2,14 @@ const { Router } = require("express");
 const { check } = require("express-validator");
 
 // controllers
-const {createContact} = require ("../controllers/contacts")
+const { createContact, listContacts } = require ("../controllers/contacts")
 const { checkValidator } = require("../middlewares/userValidate");
 
 const router = Router();
 
 const { Contact } = require('../models');
 
-router.get('/', validateJWT, adminValidate, async(req, res) => {
-    try {
-        const results = await Contact.findAll({
-            where: {
-                deletedAt: null
-            }
-        });
-        res.json({
-            results,
-            ok: true
-        })
-    } catch (error) {
-        res.json({
-            msg: error.message,
-            ok: false
-        })
-    }
-})
+router.get('/', validateJWT, adminValidate, listContacts)
 
 // POST contacts
 router.post(
