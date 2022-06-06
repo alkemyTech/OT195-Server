20946b83c3ec3f5001aa31a2d9ab850a06f16158
EnriHeller <enrique.heller@hotmail.com>
2022-06-06 00:42:24 -0300

    The send contact email method was integrated

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,9 +1,12 @@
 const { Contact } = require("../models");
+const {sendContactEmail} = require("../helpers/send-emails");
 
 const createContact = async (req, res) => {
   try {
     await Contact.create(req.body);
-    return res.status(201).json({ msg: "Contact created succesfully", ok: true });
+    await sendContactEmail(req.body.email)
+
+    return res.status(201).json({ msg: "¡Gracias por haberte contactado con nosotros!", ok: true });
   } catch (err) {
     console.log(err);
     return res.status(500).json({ msg: "Internal Server Error.", ok: false });
