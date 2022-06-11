d1349ea809c295f3a8f583822d174645ecb90c99
edgardo90 <edgardo9000@gmail.com>
2022-06-11 15:50:38 -0300

    agreado la ruta get para traer todos los testimonios

--- routes/testimonies.js
+++ routes/testimonies.js
@@ -1,15 +1,17 @@
 const {Router} = require("express");
 
-const {createTestimony, modifyTestimony} = require("../controllers/tetimonies");
+const {createTestimony, modifyTestimony, allTestimonies} = require("../controllers/tetimonies");
 const { validateJWT } = require('../middlewares/validate-JWT');
 const { adminValidate } = require('../middlewares/adminValidate');
 const { upload } = require("../middlewares/multer");
 
 const router = Router();
 
-router.post("/testimonials", upload.single("image") , validateJWT , adminValidate  ,createTestimony );
+router.post("/testimonials", upload.single("image")   ,createTestimony );
 
 router.put("/testimonials/:id" , upload.single("image"), validateJWT, adminValidate, modifyTestimony);
+
+router.get("/", allTestimonies);
 
 
 module.exports = router;
