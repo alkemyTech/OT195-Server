b48ad1485c5fb3024c0ae868e3c94f3617af8092
Sergio Rivera <sergiorive02@gmail.com>
2022-06-13 21:37:08 -0300

    feat: cloudinary integrated successfully

---
+++ controllers/uploads.js
@@ -0,0 +1,50 @@
+const { User, Entry, Member, Public, Slide, Testimony } = require("../models");
+
+const cloudinary = require("cloudinary").v2;
+cloudinary.config(process.env.CLOUDINARY_URL);
+
+const putImagenColeccion = async (req, res) => {
+  const { id, coleccion } = req.params;
+
+  try {
+    let modelo;
+    switch (coleccion) {
+      case "users":
+        modelo = await User.findOne({ where: { id, deletedAt: null } });
+        if (!modelo) {
+          return res.status(400).json({
+            msg: `User not found.`,
+          });
+        }
+
+        break;
+
+      default:
+        return res.status(500).json({ msg: "Internal server error." });
+    }
+
+    // Se limpian las imagenes previas que tenia el modelo
+    if (modelo.image) {
+      const nombreArr = modelo.image.split("/");
+      const nombre = nombreArr[nombreArr.length - 1];
+      const [public_id] = nombre.split(".");
+      cloudinary.uploader.destroy(public_id);
+    }
+
+    // Se guarda la imagen en la nube
+    const { tempFilePath } = req.files.image;
+    const { secure_url } = await cloudinary.uploader.upload(tempFilePath);
+    modelo.image = secure_url;
+
+    await modelo.save();
+
+    return res.status(200).json({ msg: "Imagen subida correctamente." });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ msg: "Error interno." });
+  }
+};
+
+module.exports = {
+  putImagenColeccion,
+};
