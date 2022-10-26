9cc37356176ac3c75efff5cc235c838fc5760ec0
edgardo90 <edgardo9000@gmail.com>
2022-10-26 15:17:36 -0300

    modificacion en create-entry.js de migrations y cree un ejemplo de seeder

--- seeders/20220603202158-create-public-data.js
+++ seeders/20220603202158-create-public-data.js
@@ -8,11 +8,12 @@ module.exports = {
         {
           name: "Somos Más",
           image:
-            "https://res.cloudinary.com/dcd14hpmu/image/upload/v1655915588/LOGO-SOMOS_MAS_mt664q.png",
+            "https://res.cloudinary.com/dcpidvrla/image/upload/v1666806349/LOGO-SOMOS_MAS_rtxsbc.png",
           phone: "1160112988",
           address: "",
           email: "somosfundacionmas@gmail.com",
           welcomeTitle: "Hola! Bienvenidx",
+          welcomeImage:"https://res.cloudinary.com/dcpidvrla/image/upload/v1666806376/Foto_5_ga4sak.jpg",
           welcomeText:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Adipiscing dignissim ac et eleifend lacus, rhoncus, dignissim sagittis. Tellus ac a, elementum ut. Tellus a morbi tincidunt ultricies malesuada eget turpis. Lacus enim non enim, velit hac turpis interdum arcu. Suspendisse at vel ultrices amet orci enim lectus porttitor ut.",
           createdAt: new Date(),
---
+++ seeders/20221026154257-demo-members.js
@@ -0,0 +1,26 @@
+//ejemplo de como hacer un seeders para que se guarde datos en la data base
+
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
